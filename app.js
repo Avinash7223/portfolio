003cae3a0cf92d46b5331c1621ccddddec63603a
Puram Avinash <95042051+Avinash7223@users.noreply.github.com>
2024-09-21 13:09:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
-let sections = document.querySelectorAll('section');
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 100;
-        let height = sec.offsetHeight;
+document.addEventListener("DOMContentLoaded", function () {
+    const sections = document.querySelectorAll("div[id]");
+    
+    const options = {
+        root: null,
+        threshold: 0.1
+    };
 
-        if (top >= offset && top < offset + height) {
-            sec.classList.add('show-animate');
-        } 
-        else {
-            sec.classList.remove('show-animate');
-        }
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+                observer.unobserve(entry.target);
+            }
+        });
+    }, options);
+
+    sections.forEach(section => {
+        section.classList.add("fade-in");
+        observer.observe(section);
     });
-};
+});
+
+
+
+function openPopup() {
+    document.getElementById("pdfModal").style.display = "block";
+}
+
+function closePopup() {
+    document.getElementById("pdfModal").style.display = "none";
+}
